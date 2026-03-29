beed02eaafaa006e4e6f0f12f94d1866b0cee44e
ABDELHADI <adjimabdelhadimi@gmail.com>
2026-03-29 19:58:51 +0200

    js 1 commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const bells = new Audio("./sounds/bell.wav");
+const startBtn = document.querySelector(".btn-start");
+const session = document.querySelector(".minutes");
+let myInterval;
+let state = true;
+
+const appTimer = () => {
+  const sessionAmount = Number.parseInt(session.textContent);
+
+  if (state) {
+    state = false;
+    let totalSeconds = sessionAmount * 60;
+
+    const updateSeconds = () => {
+      // Function code here.
+    };
+    myInterval = setInterval(updateSeconds, 1000);
+  } else {
+    alert("Session has already started.");
+  }
+};
